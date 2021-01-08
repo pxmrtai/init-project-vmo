c692a41acffd82538368bfb53fccab45a08604c7
TNT <pxmrtai97@gmail.com>
2021-01-08 14:31:28 +0700

    init project

---
+++ days-1/variable.js
@@ -0,0 +1,10 @@
+var stringVar = 'string'
+var booleanVar  = true
+var undefinedVar = undefined
+var nullVar = null
+var a;
+var b; 
+var c;
+var d;
+
+console.log(typeof stringVar, typeof booleanVar, typeof undefinedVar, typeof nullVar)
